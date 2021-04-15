4caaeab9ad3d42ebe0ab1cfdad6e72f49f317aa7
Quidmye <markmaquire@gmail.com>
2021-04-15 18:24:22 +0300

    repair Home work 14

--- hm14/script.js
+++ hm14/script.js
@@ -7,85 +7,89 @@ document.forms.texta.mytextearea.oninput = function (e) {
 }
 document.addEventListener("DOMContentLoaded", () => {
     document.forms.texta.mytextearea.value = localStorage.getItem('textarea');
-});
 
 
 // - Дана форма с инпутами, текстареа, чекбоксами, радио кнопочками, селектами и тп.
 // Пользователь вводит какие-то данные и закрывает страницу (не факт, что он заполнил всю форму).
 // Сделайте так, чтобы при следующем заходе на страницу введенные им ранее данные стояли на своих местах.
 // Сделайте ваш скрипт как можно более универсальным.
-document.addEventListener("DOMContentLoaded", () => {
+
     for (let i = 0; i < document.forms.length; i++) {
-        document.forms[i].oninput = function (e) {
-            data = [];
-            for (let elementsKey in this.elements) {
-                let value = this.elements[elementsKey].type === 'checkbox' || this.elements[elementsKey].type === 'radio' ?
-                    this.elements[elementsKey].checked : this.elements[elementsKey].value;
-                if (this.elements[elementsKey].type) {
-                    data.push({
-                        name: this.elements[elementsKey].name,
-                        type: this.elements[elementsKey].type,
-                        value: value
-                    });
-                }
-            }
-            localStorage.setItem('formData-' + this.name, JSON.stringify(data));
-        }
-        if (localStorage.getItem('formData-' + document.forms[i].name) !== null) {
-            const parsedData = JSON.parse(localStorage.getItem('formData-' + document.forms[i].name));
-            parsedData.forEach((value, key) => {
-                if (value.type === 'checkbox' || value.type === 'radio') {
-                    if (value.value) {
-                        document.forms[i][key].checked = "checked";
+        if (document.forms[i].name !== 'saveHistory' && document.forms[i].name !== 'texta' && document.forms[i].name !== 'notes') {
+            document.forms[i].oninput = function (e) {
+                data = [];
+                for (let elementsKey in this.elements) {
+                    let value = this.elements[elementsKey].type === 'checkbox' || this.elements[elementsKey].type === 'radio' ?
+                        this.elements[elementsKey].checked : this.elements[elementsKey].value;
+                    if (this.elements[elementsKey].type) {
+                        data.push({
+                            name: this.elements[elementsKey].name,
+                            type: this.elements[elementsKey].type,
+                            value: value
+                        });
                     }
-                } else if (value.type === 'textarea') {
-                    document.forms[i][key].innerHTML = value.value;
-                } else {
-                    document.forms[i][key].value = value.value;
                 }
-            })
+                localStorage.setItem('formData-' + this.name, JSON.stringify(data));
+            }
+            if (localStorage.getItem('formData-' + document.forms[i].name) !== null) {
+                const parsedData = JSON.parse(localStorage.getItem('formData-' + document.forms[i].name));
+                parsedData.forEach((value, key) => {
+                    if (value.type === 'checkbox' || value.type === 'radio') {
+                        if (value.value) {
+                            document.forms[i][key].checked = "checked";
+                        }
+                    } else if (value.type === 'textarea') {
+                        document.forms[i][key].innerHTML = value.value;
+                    } else {
+                        document.forms[i][key].value = value.value;
+                    }
+                })
+            }
         }
     }
-});
 
 
 // -Дан текстареа. В него можно ввести данные, нажать кнопку "сохранить" и они "фикисруются" (в хранилище), затем поредактировать их, затем еще поредактировать и возможно еще.....
 // Требование : хранить историю своих изменений (даже после перезагрузки страницы).
 // Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться по истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
-document.addEventListener("DOMContentLoaded", () => {
+    let index = localStorage.getItem('historyIndex') ? +localStorage.getItem('historyIndex') : 0;
     let historyOfForm = localStorage.getItem('formHistory') ? JSON.parse(localStorage.getItem('formHistory')) : [];
-    if (historyOfForm.length > 0) {
-        let index = historyOfForm.length - 1;
+    if (index > 0) {
         document.forms.saveHistory.stexts.value = historyOfForm[index];
-    } else {
-        let index = 0;
     }
     document.forms.saveHistory.goBack.onclick = function () {
+        let index = localStorage.getItem('historyIndex') ? +localStorage.getItem('historyIndex') : 0;
         if (index > 0) {
             document.forms.saveHistory.stexts.value = historyOfForm[--index];
+            localStorage.setItem('historyIndex', index);
         }
         return false;
     }
     document.forms.saveHistory.goForward.onclick = function () {
-        if (index < historyOfForm.length) {
+        let index = localStorage.getItem('historyIndex') ? +localStorage.getItem('historyIndex') : 0;
+        if (index < historyOfForm.length - 1) {
             document.forms.saveHistory.stexts.value = historyOfForm[++index];
+            localStorage.setItem('historyIndex', index);
         }
         return false;
     }
-});
-document.forms.saveHistory.onsubmit = function (e) {
-    e.preventDefault();
-    let historyOfForm = localStorage.getItem('formHistory') ? JSON.parse(localStorage.getItem('formHistory')) : [];
-    historyOfForm.push(document.forms.saveHistory.stexts.value);
-    localStorage.setItem('formHistory', JSON.stringify(historyOfForm));
-}
+
+    document.forms.saveHistory.onsubmit = function (e) {
+        e.preventDefault();
+        let historyOfForm = localStorage.getItem('formHistory') ? JSON.parse(localStorage.getItem('formHistory')) : [];
+        if (document.forms.saveHistory.stexts.value !== historyOfForm[localStorage.getItem('historyIndex')]) {
+            historyOfForm.push(document.forms.saveHistory.stexts.value);
+            localStorage.setItem('formHistory', JSON.stringify(historyOfForm));
+            localStorage.setItem('historyIndex', JSON.parse(localStorage.getItem('formHistory')).length - 1);
+        }
+    }
 
 // - Реализуйте записную книгу, хранящую данные в локальном хранилище.
 // Данные которые надо сохранять : ФИО, номер, почта, фирма, отдел, день рождения
 // Данные вводить через соответсвующую форму.
 // --Каждому контакту добавить кнопку для удаления контакта.
 // --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования, которые уже заполнены данными объекта
-document.addEventListener("DOMContentLoaded", () => {
+
     function saveNote(e) {
         e.preventDefault();
         let notesList = localStorage.getItem('notesList') ? JSON.parse(localStorage.getItem('notesList')) : [];
@@ -97,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         if (!this.parentNode.id) {
             notesList.push(data);
-        }else{
+        } else {
             notesList.splice(this.parentNode.id, 1, data);
         }
         localStorage.setItem('notesList', JSON.stringify(notesList));
